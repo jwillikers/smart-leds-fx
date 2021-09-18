2d1107c07228bb4151242707f22d5fdfc8b4b1cf
Jordan Williams <jordan@jwillikers.com>
2021-09-18 09:11:04 -0500

    Add support for the Microbit v2

--- .gdbinit
+++ .gdbinit
@@ -1,10 +1,10 @@
-target extended-remote /dev/ttyBmpGdb
-monitor swdp_scan
-attach 1
-
+# Connects GDB to OpenOCD server port
+target remote :3333
+# (optional) Unmangle function names when debugging
 set print asm-demangle on
-
-break DefaultHandler
-break rust_begin_unwind
-
+# Load your program, breaks at entry
 load
+# (optional) Add breakpoint at function
+break main
+# Continue with execution
+continue
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
 #![no_std]
 #![no_main]
 
-extern crate nb;
+use cortex_m_rt::entry;
+use panic_halt as _;
 
-extern crate panic_halt;
-
-use riscv_rt::entry;
-use longan_nano::hal::{pac, prelude::*};
-use longan_nano::led::{Led, rgb};
-use longan_nano::hal::delay::McycleDelay;
-use longan_nano::hal::spi::Pins;
+use microbit::{
+    hal::{
+        gpio::{p0::Parts, Level},
+        prelude::*,
+        spi, Timer,
+    },
+    Peripherals,
+};
 
 use core::fmt::Debug;
 use core::ops::{AddAssign, Not, SubAssign};
+use embedded_time::duration::*;
 use num_traits::PrimInt;
-use smart_leds::{hsv::{hsv2rgb, Hsv}, SmartLedsWrite, White, RGB, RGBW, brightness, RGB8};
-// use ws2812_spi::prerendered::Ws2812;
+use smart_leds::{
+    hsv::{hsv2rgb, Hsv},
+    SmartLedsWrite, RGB,
+};
 use ws2812_spi::Ws2812;
-use longan_nano::hal::spi::Spi;
-use embedded_hal::spi::MODE_0;
 
 /// The direction of the changing brightness.
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -101,90 +104,45 @@ impl<T> HsColor<T> {
     }
 }
 
-/// Configuration of the Neopixel and the soft blink effect.
-#[derive(Clone, Copy, Debug, PartialEq)]
-struct Config<T: PrimInt + Debug + SubAssign + AddAssign> {
-    /// The brightness range of the soft blink effect.
-    brightness_range: BrightnessRange<T>,
-    /// Delay between each individual brightness adjustment in milliseconds.
-    delay_ms: T,
-    /// The color of the Neopixel.
-    neopixel_color: HsColor<T>,
-    /// Be careful setting the cycles value too high when fading brightness.
-    /// You might end up with a bright white Neopixel.
-    spin_timer_cycles: u32,
-}
-
 /// A gentle, flame-like orange color.
-static RESTFUL_ORANGE: HsColor<u8> = HsColor {
+const RESTFUL_ORANGE: HsColor<u8> = HsColor {
     hue: 5,
     saturation: 255,
 };
 
 #[entry]
 fn main() -> ! {
-    let config = Config {
-        brightness_range: BrightnessRange::new(1, 254, 1),
-        delay_ms: 8,
-        neopixel_color: RESTFUL_ORANGE,
-        spin_timer_cycles: 4,
+    const DELAY: Milliseconds<u32> = Milliseconds::<u32>(8);
+    const LED_COLOR: HsColor<u8> = RESTFUL_ORANGE;
+    const NUM_LEDS: usize = 30;
+
+    let brightness_range = BrightnessRange::new(1, 254, 1);
+
+    let dp = Peripherals::take().unwrap();
+    let mut delay = Timer::new(dp.TIMER0);
+    let port0 = Parts::new(dp.P0);
+    let sck = port0.p0_17.into_push_pull_output(Level::Low).degrade();
+    let mosi = port0.p0_13.into_push_pull_output(Level::Low).degrade();
+    let miso = port0.p0_01.into_floating_input().degrade();
+    let pins = spi::Pins {
+        sck,
+        miso: Some(miso),
+        mosi: Some(mosi),
     };
-    let dp = pac::Peripherals::take().unwrap();
-    let mut rcu = dp
-        .RCU
-        .configure()
-        .ext_hf_clock(8.mhz())
-        .sysclk(108.mhz())
-        .freeze();
-    let mut afio = dp.AFIO.constrain(&mut rcu);
-    // let mut rcu = dp.RCU.configure().freeze();
+    let spi = spi::Spi::new(dp.SPI0, pins, spi::Frequency::M2, spi::MODE_0);
 
-    let gpioa = dp.GPIOA.split(&mut rcu);
-    // let gpioc = dp.GPIOC.split(&mut rcu);
-
-    let mut delay = McycleDelay::new(&rcu.clocks);
-
-    let sck = gpioa.pa5.into_alternate_push_pull();
-    let miso = gpioa.pa6.into_floating_input();
-    let mosi = gpioa.pa7.into_alternate_push_pull();
-
-    let spi = Spi::spi0(dp.SPI0, (sck, miso, mosi), &mut afio, MODE_0, 3_000_000.hz(), &mut rcu);
-
-    // let mut delay = Delay::new(core.SYST, &mut clocks);
-    // let mut pins = Pins::new(peripherals.PORT);
-    //
-    // let spi = spi_master(
-    //     &mut clocks,
-    //     3_000_000u32.hz(),
-    //     peripherals.SERCOM1,
-    //     &mut peripherals.MCLK,
-    //     pins.sck,
-    //     pins.mosi,
-    //     pins.miso,
-    //     &mut pins.port,
-    // );
-
-    const NUM_LEDS: usize = 8;
-    // let mut buffer: [u8; 16 * NUM_LEDS + 40] = [0; 16 * NUM_LEDS + 40];
-    // let mut ws = Ws2812::new_sk6812w(spi, &mut buffer);
-    let mut ws = Ws2812::new_sk6812w(spi);
+    let mut ws = Ws2812::new(spi);
 
     loop {
-        for j in config.brightness_range {
+        for j in brightness_range {
             let rgb: RGB<u8> = hsv2rgb(Hsv {
-                hue: config.neopixel_color.hue,
-                sat: config.neopixel_color.saturation,
+                hue: LED_COLOR.hue,
+                sat: LED_COLOR.saturation,
                 val: j,
             });
-            let rgbw: RGBW<u8> = RGBW {
-                r: rgb.r,
-                g: rgb.g,
-                b: rgb.b,
-                a: White(j / 20),
-            };
-            let data = [rgbw; NUM_LEDS];
+            let data = [rgb; NUM_LEDS];
             ws.write(data.iter().cloned()).unwrap();
-            delay.delay_ms(config.delay_ms);
+            delay.delay_ms(DELAY.integer());
         }
     }
 }
